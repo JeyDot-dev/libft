fd13a7fe5a7309ef9a0f20f3cb2919937dc02934
JeyDot <jsousa-a@student.42lausanne.ch>
2022-10-29 10:49:41 +0200

    strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,12 +6,25 @@
 /*   By: jsousa-a <jsousa-a@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 00:23:57 by jsousa-a          #+#    #+#             */
-/*   Updated: 2022/10/26 00:46:04 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2022/10/29 10:48:28 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*strdup(const char *s, size_t n)
+char	*ft_strdup(const char *s)
 {
+	char	*scpy;
+	int		i;
 
+	i = 0;
+	scpy = (char *) malloc(sizeof(*s) * (ft_strlen(s) + 1));
+	if (!scpy)
+		return (scpy);
+	while (s[i])
+	{
+		scpy[i] = s[i];
+		i++;
+	}
+	scpy[i] = 0;
+	return (scpy);
 }
