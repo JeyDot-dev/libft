7bd487ec8f66402d38924ab9bc6ecf5c6b7cdd7a
JeyDot <jsousa-a@student.42lausanne.ch>
2022-10-30 16:34:49 +0100

    strjoin unfinished

---
+++ ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsousa-a <jsousa-a@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 13:11:25 by jsousa-a          #+#    #+#             */
+/*   Updated: 2022/10/30 15:48:35 by jsousa-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len;
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	j = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(sizeof(*str) * (len + 1));
+	if (!str)
+		return (str);
+	while (s1[j])
+	{
+		str[i] = s1[j];
+		i++;
+		j++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = 0;
+	return (str);
+}
