a53592f33f85f0ee1d988badf52a9ca242306bdf
JeyDot <jsousa-a@student.42lausanne.ch>
2022-11-08 17:33:05 +0100

    strtrim 99%

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,15 +6,15 @@
 /*   By: jsousa-a <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:57:12 by jsousa-a          #+#    #+#             */
-/*   Updated: 2022/11/04 17:46:43 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2022/11/08 15:09:01 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int		ft_check(char c, char const *set)
+#include "libft.h"
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+/*int		ft_check(char c, char const *set)
 {
 	int		flag;
 	size_t	i;
@@ -29,38 +29,38 @@ int		ft_check(char c, char const *set)
 		return (1);
 	else
 		return (0);
+}*/
+int	ft_start(int i, char const *s1, char const *set)
+{
+	i = ft_strlen(s1);
+
+	if (i > 0)
+		i--;
+	while (i > 0 && ft_strchr(set, s1[i]) != NULL)
+		i--;
+	return (i);
 }
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
 	size_t	j;
-	size_t size;
 	char	*str;
 
-	j = strlen(s1); //TODO Change to ft_strlen
+	j = 0;
+	j = ft_start(j, s1, set);
 	i = 0;
-	if (j > 0)
-		j--;
-	while (j > 0 && ft_check(s1[j], set) == 1)
-		j--;
-	while (s1[i] && ft_check(s1[i], set) == 1)
+	while (s1[i] && ft_strchr(set, s1[i]) != NULL)
 		i++;
-	size = j - i + 1;
-	str = malloc(sizeof(*str) * (size + 1));
+	str = malloc(sizeof(*str) * (j - i + 2));
 	if (!str)
 		return (NULL);
-	while (i <= j)
-	{
-		*str = s1[i];
-		str++;
-		i++;
-	}
-	*str = 0;
+	ft_strlcpy(str, &s1[i], j - i + 2);
 	return (str);
 }
 
-int	main(int ac, char **av)
+/*int	main(int ac, char **av)
 {
 	(void) ac;
 	printf("\n%s", ft_strtrim(av[1], av[2]));
-}
+}*/
