019b4c74d79d71f103fbab2db6deeb0d67df2b64
JeyDot <jsousa-a@student.42lausanne.ch>
2022-11-11 21:11:03 +0100

    ft_split fonctionnel (need debug)

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jsousa-a <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 14:37:39 by jsousa-a          #+#    #+#             */
-/*   Updated: 2022/11/11 15:10:47 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2022/11/11 21:03:55 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ size_t	ft_ct(char const *s, char c)
 
 	i = 0;
 	ct = 0;
+	if (c == 0)
+		return (1);
 	while (s[i])
 	{
 		if (s[i] != c)
@@ -30,16 +32,62 @@ size_t	ft_ct(char const *s, char c)
 	}
 	return (ct);
 }
+size_t	ft_lenct(char const *s, char c, int i)
+{
+	int	ct;
 
+	ct = 0;
+	if (c == 0)
+		return (ft_strlen(s));
+	while (s[i] != c && s[i])
+	{
+		i++;
+		ct++;
+	}
+	return (ct);
+}
 char	**ft_split(char const *s, char c)
 {
 	char	**strarr;
 	size_t	i;
+	size_t	j;
+	size_t	jj;
+	size_t	strct;
 
+	i = 0;
+	jj = 0;
+	if (s == NULL)
+		return (NULL);
+	strct = ft_ct(s, c);
+	strarr = malloc(sizeof(*strarr) * (strct + 1));
+	if (!strarr)
+		return (NULL);
+	while (i < strct)
+	{
+		j = 0;
+		while (s[jj] && s[jj] == c)
+			jj++;
+		strarr[i] = malloc(sizeof(**strarr) * (ft_lenct(s, c, jj) + 1));
+		while (s[jj] && s[jj] != c)
+		{
+			strarr[i][j] = s[jj];
+			j++;
+			jj++;
+		}
+		strarr[i][j] = 0;
+		i++;
+	}
+	strarr[i] = NULL;
+	return (strarr);
 }
 int	main(int ac, char **av)
 {
-	void 
-	printf("%i", ft_split(av[1], av[2]));
-
+	(void) ac;
+	size_t	i;
+	i = 0;
+	while (i <= ft_ct(av[1], av[2][0]))
+	{
+		printf("str %li : %s\n", i, ft_split(av[1], av[2][0])[i]);
+		i++;
+	}
 }
