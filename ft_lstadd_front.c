1c05a381722c47bc6823a21b98294824848e6fef
JeyDot <jsousa-a@student.42lausanne.ch>
2022-11-21 11:45:43 +0100

    finished lstsize.c

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,19 +6,26 @@
 /*   By: jsousa-a <jsousa-a@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 23:34:22 by jsousa-a          #+#    #+#             */
-/*   Updated: 2022/11/21 00:44:23 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2022/11/21 10:35:53 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!lst && !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
 
-int	main(void)
+/*int	main(void)
 {
 	t_list	lel;
 	t_list	*tst;
 	t_list	**tst2;
+	t_list	*tst3;
 	char a;
 	char *b;
 
@@ -26,10 +33,13 @@ int	main(void)
 	b = &a;
 	lel.content = b;
 	tst = &lel;
+	tst3 = lel.content;
 	tst2 = tst->content;
 
 
 	write(1, lel.content, 1);
+	write(1, "\n", 1);
+	write(1, tst3, 1);
 	write(1, "\n", 1);
 	write(1, tst2, 1);
 	write(1, "\n", 1);
@@ -37,4 +47,4 @@ int	main(void)
 	write(1, lel.content, 1);
 	write(1, "\n", 1);
 	write(1, tst2, 1);
-}
+}*/
